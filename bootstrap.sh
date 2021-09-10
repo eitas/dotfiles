55152261c3cad5fb73802644e3289b800cf368c4
Max Thomas <max.thomas@moneydashboard.com>
2021-09-10 17:53:20 +0100

    Adding Brave browser

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,14 +48,20 @@ apt_installs() {
     echo "updating package manager" | tee -a $LOGFILE
     echo "------------------------" | tee -a $LOGFILE
     apt-get update -y
-    echo "------------------" | tee -a $LOGFILE
-    echo "upgrading packages" | tee -a $LOGFILE
-    echo "------------------" | tee -a $LOGFILE
-    apt-get update -y
     echo "------------------------" | tee -a $LOGFILE
     echo "package updates complete" | tee -a $LOGFILE
     echo "------------------------" | tee -a $LOGFILE
 
+    # brave
+    echo "-----------------" | tee -a $LOGFILE
+    echo "installing chrome" | tee -a $LOGFILE
+    echo "-----------------" | tee -a $LOGFILE
+    sudo apt install -y apt-transport-https curl
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update -y
+    sudo apt install -y brave-browser 
+ 
     # chrome
     echo "-----------------" | tee -a $LOGFILE
     echo "installing chrome" | tee -a $LOGFILE
