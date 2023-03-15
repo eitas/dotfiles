6937b99d2074c17fc6ba5248cbb50e8176da6da0
Max Thomas <4568748+eitas@users.noreply.github.com>
2023-03-15 14:21:43 +0000

    Commenting out tab luasnip for nvim-cmp as this was causing errors in standard files, presumably because of an misconfiguration in my setup of nvim-cmp.  Updating telescope to simplify the syntax

--- nvim/lua/conf/nvim-cmp.lua
+++ nvim/lua/conf/nvim-cmp.lua
@@ -20,8 +20,8 @@
       ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
+--        elseif luasnip.expand_or_jumpable() then
+ --         luasnip.expand_or_jump()
         else
           fallback()
         end
--- nvim/lua/conf/telescope.lua
+++ nvim/lua/conf/telescope.lua
@@ -5,9 +5,15 @@ require('telescope').setup({
 })
 require('telescope').load_extension('fzf')
 
-vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').git_files()<cr>", {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", {noremap = true})
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input('Grep > ') })
+end)
+  
 -- Need to study the next one before enabling.  Need to get LSPs setup
 -- vim.api.nvim_set_keymap('n', '<leader>gr', "<cmd>lua require('telescope.builtin').lsp_references()<cr>", {noremap = true})
 
